b16727d593692fd913e57811d54578633907bf70
rhu-machado <rhu.machado16@gmail.com>
2022-06-22 16:13:02 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,14 @@
+let nome = 'Ebby'
+let number = 31
+let ano = 2022 - number
+
+console.log(ano + number)
+
+console.log('Estou mexendo no console.log')
+
+let quartaFeira = true
+
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
+
